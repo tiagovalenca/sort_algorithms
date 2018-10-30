3d98313b7b0b4e7a7ea776690026681c57b37e2f
tiagovalenca <tlv@cesar.school>
2018-10-30 09:31:32 -0300

    Update heapSort.c

--- heapSort.c
+++ heapSort.c
@@ -35,9 +35,9 @@ void heapSort(int vetor[], int tamanho) {
       pai = i;
       filho = i * 2 + 1;
       while (filho < tamanho) {
-          if ((filho + 1 < tamanho)  &&  (vetor[filho + 1] > vetor[filho]))
+          if ((filho + 1 < tamanho)  &&  (vetor[filho + 1] < vetor[filho]))
               filho++;
-          if (vetor[filho] > t) {
+          if (vetor[filho] < t) {
              vetor[pai] = vetor[filho];
              pai = filho;
              filho = pai * 2 + 1;
